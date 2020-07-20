64f3332a0a1b3035611949d0437d6f36893aafa5
Vento <Michael.vento@student.hepl.be>
2020-07-20 22:16:54 +0200

    add somes final changes in css, add details page

---
+++ details.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description"
+          content="Photographer website where we can participate in exhibitions or read thousands of books">
+    <meta name="keywords"
+          content="Description, Photographer, freelance, HTML, CSS, JS, Javascript, PHP">
+    <meta name="author"
+          content="Michael Vento">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta name="fb:page_id"
+          content="3158"/>
+    <meta name="og:email"
+          content="vento.michael0705@hotmail.com"/>
+    <meta name="og:locality"
+          content="JUPRELLE"/>
+    <meta name="og:region"
+          content="WALLONE"/>
+    <meta name="og:postal-code"
+          content="4450"/>
+    <meta name="og:country-name"
+          content="BELGIUM"/>
+    <link rel="stylesheet"
+          href="http://localhost:8080/src/css/style.css"
+          type="text/css">
+    <title>IMS &vert; Home</title>
+</head>
+<body>
+<header>
+    <h1 itemscope itemtype="https://schema.org/ImageObject" class="hidden">
+        Site internet d'Image sans nom
+    </h1>
+    <input class="burger" id="burger" type="checkbox"/>
+    <label class="burger" for="burger">Menu
+        <span></span>
+        <span></span>
+        <span></span>
+    </label>
+    <nav>
+        <h2 class="hidden">
+            Navigation principale
+        </h2>
+        <ul>
+            <li class="listLogo">
+                <a itemprop="url"
+                   href="http://localhost:8080/"
+                   title="Home page"
+                   class="logo">Back to home
+                    <svg enable-background="new 0 0 637.1 400.2" viewBox="0 0 637.1 400.2"
+                         xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <clipPath id="a">
+                            <path d="m26.3 25.8v7.4h-26.4v-33.2h8.9v25.8z"/>
+                        </clipPath>
+                        <clipPath id="b">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="c">
+                            <path d="m29.8 10.5h-.6c-1-.1-2.2-.7-2.2-1.7s1.4-1.9 3.1-1.9 3 .9 3 2.1c0 1.5-2 3.4-4.4 4.6h-2.4c1.4-.5 2.9-2.2 2.9-3z"/>
+                        </clipPath>
+                        <clipPath id="d">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="e">
+                            <path d="m503.5 0h6.8v33.2h-6.8z"/>
+                        </clipPath>
+                        <clipPath id="f">
+                            <path d="m545.8.3v30.3h-6.8v-7.4c0-7.2.2-10.7.2-10.7s-2.3 4.2-2.6 4.6l-3.4 6.1h-3.6l-3.4-6.1c-.2-.4-2.5-4.6-2.6-4.6s.2 3.6.2 10.7v7.4h-6.8v-30.3h6.5l5.4 9.2c2.1 3.6 2.4 4.4 2.5 4.4s.4-.8 2.5-4.4l5.4-9.2h6.5z"/>
+                        </clipPath>
+                        <clipPath id="g">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="h">
+                            <path d="m565.5 19.4-2-5.9c-.6-1.7-1.4-4.5-1.4-4.5s-.8 2.7-1.4 4.5l-2 5.9zm2.2 6.4h-11.3l-1.6 4.8h-7.4l11.5-30.3h6.5l11.5 30.3h-7.4z"/>
+                        </clipPath>
+                        <clipPath id="i">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="j">
+                            <path d="m597.9 28.7c-1.7 1.9-5 3.1-7.8 3.1-8.2 0-14.9-6.6-14.9-15.8 0-9.1 6.6-15.8 14.9-15.8 5.6 0 10.4 3.1 13 8.1l-7 1.9c-1.4-1.7-3.8-2.8-6-2.8-4.8 0-7.7 3.6-7.7 8.6 0 5.3 3.4 8.7 8.1 8.7 4.6 0 6.6-2.5 7.4-4.8h-7.5v-5.9h13.4v17.1h-5.9c0-1.9.1-2.3 0-2.4z"/>
+                        </clipPath>
+                        <clipPath id="k">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="l">
+                            <path d="m637.1 25.8v7.4h-26.5v-33.2h26.2v7.5h-17.5v5.4h13.7v7h-13.7v5.9z"/>
+                        </clipPath>
+                        <clipPath id="m">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="n">
+                            <path d="m18.1 377.2c-.8-2.7-3.2-3.9-5.4-3.9-2.3 0-3.6 1.1-3.6 2.3 0 2 3.2 2.7 7 4.1 4.7 1.6 9.6 4.2 9.6 9.4 0 5.5-5 9.4-12.3 9.4-6.6 0-11.3-3.5-13.3-9.1l7.2-1.9c.8 2.3 3.1 4.6 6.4 4.6 2.7 0 4-1.2 4-2.7 0-2.4-3.7-3-8.2-4.7-3.8-1.4-8.1-3.4-8.1-8.7 0-4.7 5-9 11.8-9 6 0 10.8 3.5 12.3 8.3z"/>
+                        </clipPath>
+                        <clipPath id="o">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="p">
+                            <path d="m44.8 386.7-2.3-5.9c-.7-1.7-1.5-4.4-1.5-4.4s-.9 2.7-1.5 4.4l-2.3 5.9zm2.4 6.4h-12.5l-1.8 4.7h-8.1l12.6-30.2h7.2l12.6 30.2h-8.2z"/>
+                        </clipPath>
+                        <clipPath id="q">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="r">
+                            <path d="m87.1 367.6v30.2h-7.1l-6.4-8.4c-2.9-3.8-7.1-9.5-7.2-9.5v11.3 6.7h-7.6v-30.2h7.1l6.7 8.6c2 2.6 6.8 9.1 6.9 9.1s0-5.3 0-10.8v-6.9h7.6z"/>
+                        </clipPath>
+                        <clipPath id="s">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="t">
+                            <path d="m107.9 377.2c-.8-2.7-3.2-3.9-5.4-3.9-2.3 0-3.6 1.1-3.6 2.3 0 2 3.2 2.7 7 4.1 4.7 1.6 9.6 4.2 9.6 9.4 0 5.5-5 9.4-12.3 9.4-6.6 0-11.3-3.5-13.3-9.1l7.2-1.9c.8 2.3 3.1 4.6 6.4 4.6 2.7 0 4-1.2 4-2.7 0-2.4-3.7-3-8.2-4.7-3.8-1.4-8.1-3.4-8.1-8.7 0-4.7 5-9 11.8-9 6 0 10.8 3.5 12.3 8.3z"/>
+                        </clipPath>
+                        <clipPath id="u">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="v">
+                            <path d="m565.7 368.8v30.2h-7.1l-6.4-8.4c-2.9-3.8-7.1-9.5-7.2-9.5v11.3 6.7h-7.6v-30.2h7.1l6.7 8.6c2 2.6 6.8 9.1 6.9 9.1s0-5.3 0-10.8v-6.9h7.6z"/>
+                        </clipPath>
+                        <clipPath id="w">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="x">
+                            <path d="m585.5 392.5c5.1 0 8.5-3.5 8.5-8.5 0-5.1-3.4-8.5-8.5-8.5s-8.5 3.4-8.5 8.5c0 5 3.4 8.5 8.5 8.5m0-24.3c9 0 16.4 6.6 16.4 15.7s-7.3 15.7-16.4 15.7c-9 0-16.3-6.6-16.3-15.7-.1-9.1 7.2-15.7 16.3-15.7"/>
+                        </clipPath>
+                        <clipPath id="y">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <clipPath id="z">
+                            <path d="m636.8 368.8v30.2h-7.4v-7.4c0-7.2.3-10.7.2-10.7s-2.6 4.1-2.8 4.6l-3.7 6h-3.9l-3.7-6c-.3-.4-2.8-4.6-2.8-4.6s.2 3.5.2 10.7v7.4h-7.4v-30.2h7.1l5.9 9.1c2.3 3.6 2.7 4.4 2.7 4.4s.4-.8 2.7-4.4l5.9-9.1z"/>
+                        </clipPath>
+                        <clipPath id="A">
+                            <path d="m-27.2-86.7h694.1v650.7h-694.1z"/>
+                        </clipPath>
+                        <g clip-path="url(#a)">
+                            <path clip-path="url(#b)" d="m-3.5-3.4h33.2v40h-33.2z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#c)">
+                            <path clip-path="url(#d)" d="m22.9 3.4h13.6v13.6h-13.6z" fill="#feeb19"/>
+                        </g>
+                        <path clip-path="url(#e)" d="m500.1-3.4h13.6v40h-13.6z" fill="#feeb19"/>
+                        <g clip-path="url(#f)">
+                            <path clip-path="url(#g)" d="m513.8-3.1h35.4v37.1h-35.4z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#h)">
+                            <path clip-path="url(#i)" d="m544-3.1h36.2v37.1h-36.2z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#j)">
+                            <path clip-path="url(#k)" d="m571.9-3.1h35.3v38.3h-35.3z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#l)">
+                            <path clip-path="url(#m)" d="m607.3-3.4h33.2v40h-33.2z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#n)">
+                            <path clip-path="url(#o)" d="m-3.3 363.6h32.4v38.2h-32.4z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#p)">
+                            <path clip-path="url(#q)" d="m21.4 364.2h39.1v37h-39.1z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#r)">
+                            <path clip-path="url(#s)" d="m55.4 364.2h35v37h-35z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#t)">
+                            <path clip-path="url(#u)" d="m86.5 363.6h32.4v38.2h-32.4z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#v)">
+                            <path clip-path="url(#w)" d="m534 365.5h35v37h-35z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#x)">
+                            <path clip-path="url(#y)" d="m565.7 364.9h39.5v38.2h-39.5z" fill="#feeb19"/>
+                        </g>
+                        <g clip-path="url(#z)">
+                            <path clip-path="url(#A)" d="m601.9 365.5h38.2v37h-38.2z" fill="#feeb19"/>
+                        </g>
+                    </svg>
+                </a>
+            </li>
+            <li class="listItems">
+                <ul>
+                    <li class="currentItem"><a href="index.php">Accueil</a></li>
+                    <li><a href="photographie.php">Photographie</a></li>
+                    <li><a href="photographie.php">Bibliothèque</a></li>
+                    <li><a href="about.php">À propos</a></li>
+                </ul>
+            </li>
+            <li class="listItemContact">
+                <a href="contact.php">Contact</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="containerAboutUs">
+        <div class="containerTextAboutUs">
+            <h2>
+                Titre
+            </h2>
+            <p class="aboutUsParagraph">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus aspernatur consectetur,
+                consequuntur delectus distinctio error esse eum hic ipsam labore laboriosam molestias nihil nisi quae
+                quod rerum tempora totam.
+            </p>
+            <a class="ctaContact" href="#">
+                <span>Nous contactez</span>
+            </a>
+        </div>
+        <div class="containerImageBoxRight">
+            <img class="imageBoxRight" src="src/img/expo/affiches_001.jpg" alt="Representing the front of the building">
+        </div>
+    </section>
+    <div class="svgScroll">
+        <svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+             x="0px" y="0px"
+             viewBox="0 0 250 12" style="enable-background:new 0 0 250 12;" xml:space="preserve">
+            <style type="text/css">
+            .st0 {
+                fill: #FCEC43;
+            }
+
+            .st1 {
+                font-family: 'Novecentowide-Bold';
+            }
+
+            .st2 {
+                font-size: 12px;
+            }
+            </style>
+                <g>
+                    <rect x="66" y="3" class="st0" width="100" height="5"/>
+                </g>
+                <text transform="matrix(1 0 0 1 6.5054 10.2529)" class="st1 st2">Scroll</text>
+        </svg>
+    </div>
+
+    <div class="scrolling-wrapper-flexbox">
+        <div class="card">
+            <img src="src/img/expo/affiches_001.jpg"/>
+        </div>
+        <div class="card">
+            <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+        </div>
+        <div class="card">
+            <img src="src/img/expo/affiches_001.jpg"/>
+        </div>
+        <div class="card">
+            <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+        </div>
+        <div class="card">
+            <img src="src/img/expo/affiches_001.jpg"/>
+        </div>
+        <div class="card">
+            <iframe width="420" height="315"
+                    src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
+        </div>
+    </div>
+</main>
+<footer>
+    <section>
+        <h2>
+            Contact
+        </h2>
+        <p>
+            Rue vivegnis, 46
+        </p>
+        <span><a href="mailto:oli@oliviercornil.be">oli@oliviercornil.be</a></span>
+        <span><a href="tel:32494827254">+ 324 948 272 54</a></span>
+    </section>
+    <section>
+        <h2>
+            Suivez nous
+        </h2>
+        <span><a href="https://www.facebook.com/limagesansnom">Facebook</a></span>
+        <span><a href="#">Twitter</a></span>
+    </section>
+    <section>
+        <h2>
+            Newsletter
+        </h2>
+        <p>
+            Ne manquez plus aucune importante exposition ! Abonnez-vous à cette newsletter.
+        </p>
+    </section>
+</footer>
+</body>
+</html>
